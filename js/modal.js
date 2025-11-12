6c1baecc3a694651aaf319819d2f880c7dd94cbf
Vito <chasovitinvital@gmail.com>
2025-11-12 13:18:33 +0600

    Add modal timer functionality

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 const modalBtn = document.querySelector('.modal__button')
 const modal = document.querySelector('.modal')
+const closing = document.querySelector('.modal__close')
 
 modalBtn.addEventListener('click', () => {
     modal.style.display = 'flex'
@@ -7,7 +8,8 @@ modalBtn.addEventListener('click', () => {
 
 modal.addEventListener('click', (event)=> {
     const modalContent = event.target.closest('.modal__inner')
-if (!modalContent){
+    const isClose = event.target.closest('.modal__close')
+if (!modalContent || isClose){
     modal.style.display = ''
 }
 })
